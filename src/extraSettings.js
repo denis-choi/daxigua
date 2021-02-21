e2d125134f1628dab439da9cf765155448e5dcb9
Denis Choi <zommer11@gmail.com>
2021-02-22 04:48:48 +0900

    무적 수박으로 설정 변경

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -6,20 +6,20 @@
 let extraScore = 1;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 0;
+let firstFruit = 5;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = false;
 
 // 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
-const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
-const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
+const minRandomFruitNum = reverseLevelUp ? 6 : 5; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
+const maxRandomFruitNum = reverseLevelUp ? 11 : 6; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
 let setFruits = {
   // 指定前几次生成的水果，可填入任意数量的数字，0 为葡萄，9 为半个西瓜
-  startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 0, 1, 2, 2, 3],
+  startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [5, 5, 6, 6, 7, 7],
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
